d224680e257010ee4e31f993db23e8d50f402e45
MATHEUS VIEIRA <u21252@LAPA19.academico.cotuca.unicamp.br>
2023-11-14 12:04:18 -0300

    put  ajustado

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ app.put('/usuarios/alterar/:id', async (req, res) => {
             return res.status(500).json({ error: 'Erro ao atualizar no banco de dados' });
         }
 
-        return res.status(200).json('Usuário alterado com sucesso!');
+        return res.status(200).json(updateduser);
     } catch (error) {
         console.error('Erro geral:', error);
         return res.status(500).json({ error: 'Erro geral' });
